1f73d97fd9ba2ab519006d775154970552e04b77
Suraj Narwade <surajnarwade353@gmail.com>
2017-11-21 20:33:09 +0530

    Added file download

--- blocks.js
+++ blocks.js
@@ -2,26 +2,38 @@ Blockly.Blocks['version'] = {
   init: function() {
     this.appendStatementInput("version")
         .setCheck(null)
-        .appendField(new Blockly.FieldTextInput("2"), "version");
+        .appendField(new Blockly.FieldTextInput("1"), "version");
     this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
     this.setColour(150);
     this.setTooltip('version 2');
     this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/');
   }
 };
 
-Blockly.Blocks['service_name'] = {
-  init: function() {
-    this.appendStatementInput("service_name")
-        .setCheck(null)
-        .appendField(new Blockly.FieldTextInput("web"), "service_name");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-    this.setColour(170);
-    this.setTooltip('service name goes here');
-    this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/#service-configuration-reference');
-  }
+
+Blockly.Blocks['main'] = {
+    init: function() {
+        this.appendStatementInput("main")
+            .setCheck(null);
+        this.setColour(230);
+        this.setTooltip("main");
+        this.setHelpUrl("main");
+    }
+};
+
+Blockly.Blocks['image'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("image")
+            .appendField(new Blockly.FieldTextInput("image"), "image");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("image");
+        this.setHelpUrl("image");
+    }
 };
 
 Blockly.Blocks['image_name'] = {
@@ -38,11 +50,11 @@ Blockly.Blocks['image_name'] = {
   }
 };
 
-Blockly.Blocks['ports'] = {
+Blockly.Blocks['ports_mappings'] = {
 init: function() {
-    this.appendStatementInput("ports")
+    this.appendStatementInput("ports_mappings")
         .setCheck(null)
-        .appendField("ports");
+        .appendField("ports_mappings");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(195);
@@ -51,6 +63,18 @@ init: function() {
   }
 };
 
+Blockly.Blocks['env_mappings'] = {
+    init: function() {
+        this.appendStatementInput("env_mappings")
+            .setCheck(null)
+            .appendField("env_mappings");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(195);
+        this.setTooltip('');
+        this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/#ports');
+    }
+};
 
 Blockly.Blocks['page_text'] = {
   init: function() {
@@ -63,31 +87,18 @@ Blockly.Blocks['page_text'] = {
   }
 };
 
-Blockly.Blocks['restart'] = {
-  init: function() {
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_CENTRE)
-        .appendField("restart")
-        .appendField(new Blockly.FieldTextInput("always"), "restart");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
-};
 
 Blockly.Blocks['container_name'] = {
-  init: function() {
-    this.appendValueInput("container_name")
-        .setCheck(null)
-        .appendField("container_name");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(120);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
+    init: function() {
+        this.appendDummyInput()
+            .appendField("container_name")
+            .appendField(new Blockly.FieldTextInput("name"), "container_name");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("container_name");
+        this.setHelpUrl("container_name");
+    }
 };
 
 Blockly.Blocks['labels'] = {
@@ -115,92 +126,97 @@ Blockly.Blocks['label_output'] = {
   }
 };
 
-Blockly.Blocks['your_text'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("your text"), "your_text");
-    this.setOutput(true, null);
-    this.setColour(330);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
+
+Blockly.Blocks['containers'] = {
+    init: function () {
+        this.appendStatementInput("containers")
+            .setCheck(null)
+            .appendField("containers");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(240);
+        this.setTooltip("containers");
+        this.setHelpUrl("containers");
+    }
 };
 
-Blockly.Blocks['environment'] = {
-  init: function() {
-    this.appendStatementInput("environment")
-        .setCheck(null)
-        .appendField("environment");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(195);
-    this.setTooltip('');
-    this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/#environment');
-  }
+Blockly.Blocks['services'] = {
+    init: function() {
+        this.appendStatementInput("services")
+            .setCheck(null)
+            .appendField("services");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("services");
+        this.setHelpUrl("services");
+    }
 };
 
-Blockly.Blocks['args'] = {
-init: function() {
-    this.appendStatementInput("args")
-        .setCheck(null)
-        .appendField("args");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(195);
-    this.setTooltip('');
-    this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/#ports');
-  }
+Blockly.Blocks['name'] = {
+    init: function() {
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_CENTRE)
+            .appendField("name")
+            .appendField(new Blockly.FieldTextInput("name"), "name");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(240);
+        this.setTooltip("name");
+        this.setHelpUrl("name");
+    }
 };
 
-Blockly.Blocks['env_file'] = {
-  init: function() {
-    this.appendValueInput("env_file")
-        .setCheck("String")
-        .appendField("env_file");
-    this.setInputsInline(false);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
-    this.setTooltip('Image name goes here');
-    this.setHelpUrl('https://docs.docker.com/compose/compose-file/compose-file-v2/#image');
-  }
+Blockly.Blocks['controller'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("controller")
+            .appendField(new Blockly.FieldDropdown([["Deployment","Deployment"], ["Job","Job"], ["DeploymentConfig","DeploymentConfig"]]), "controller");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("controller");
+        this.setHelpUrl("controller");
+    }
 };
 
-Blockly.Blocks['links'] = {
-  init: function() {
-    this.appendStatementInput("links")
-        .setCheck(null)
-        .appendField("links");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(330);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
+Blockly.Blocks['service_type'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("service_type")
+            .appendField(new Blockly.FieldDropdown([["NodePort","NodePort"], ["LoadBalancer","LoadBalancer"], ["ClusterIP","ClusterIP"]]), "service_type");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("service_type");
+        this.setHelpUrl("service_type");
+    }
 };
 
-Blockly.Blocks['cap_add'] = {
-  init: function() {
-    this.appendStatementInput("cap_add")
-        .setCheck(null)
-        .appendField("cap_add");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(330);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
+Blockly.Blocks['service_name'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("service_name")
+            .appendField(new Blockly.FieldTextInput("name"), "service_name");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("service_name");
+        this.setHelpUrl("service_name");
+    }
 };
 
-Blockly.Blocks['cap_drop'] = {
-  init: function() {
-    this.appendStatementInput("cap_drop")
-        .setCheck(null)
-        .appendField("cap_drop");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(330);
-    this.setTooltip('');
-    this.setHelpUrl('');
-  }
+Blockly.Blocks['env'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldTextInput("key"), "key")
+            .appendField("=")
+            .appendField(new Blockly.FieldTextInput("value"), "value");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("env");
+        this.setHelpUrl("env");
+    }
 };
+
--- javascript.js
+++ javascript.js
@@ -1,37 +1,10 @@
-
 Blockly.JavaScript['version'] = function(block) {
   var text_version_name = block.getFieldValue('version');
   var statements_version = Blockly.JavaScript.statementToCode(block, 'version');
-
-  var dict12 = {};
-        a = statements_version.split("!");
-
-
-        aq = a[a.length - 2];
-
-        b = JSON.parse(aq);
-
- dict12['version'] = text_version_name;
-dict12['services'] = b;
-//dict12['Services'] = {"null":"22", "redis":"22", "redis":"22"};
-//dict['Services'] = {web: "", red: "", fg: ""};
-  code = JSON.stringify(dict12);
-  return code;
-};
-
-
-
-var dicto = {};
-
-Blockly.JavaScript['service_name'] = function(block) {1
-  var text_service_name = block.getFieldValue('service_name');
-  var statements_service_name = Blockly.JavaScript.statementToCode(block, 'service_name');
-
-
-    a = statements_service_name.split("!");
-
-
-         function jsonConcat(o1, o2) {
+  //  console.log(statements_version);
+   var dict12 = {};
+         a = statements_version.split("!");
+    function jsonConcat(o1, o2) {
           for (var key in o2) {
            o1[key] = o2[key];
           }
@@ -47,60 +20,206 @@ Blockly.JavaScript['service_name'] = function(block) {1
 
            }
 
+      //   aq = a[a.length - 2];
 
+      //   b = JSON.parse(aq);
 
-   dicto[text_service_name] = config;
-
- var code = JSON.stringify(dicto);
-// return "suraj";
- return code+"!";
+  dict12['filename'] = text_version_name;
+ dict12['data'] = config;
+//
+// //dict12['Services'] = {"null":"22", "redis":"22", "redis":"22"};
+// //dict['Services'] = {web: "", red: "", fg: ""};
+   code = JSON.stringify(dict12);
+  //  code = text_version_name+statements_version;
+   // console.log(text_version_name, statements_version);
+  return code+"!";
 };
 
-Blockly.JavaScript['image_name'] = function(block) {
-  var value_image_name = Blockly.JavaScript.valueToCode(block, 'image_name', Blockly.JavaScript.ORDER_ATOMIC);
-    value_image_name = value_image_name.replace(/[{()}]/g, '');
- var dict = {};
-  dict['image'] = value_image_name;
-  var code = JSON.stringify(dict);
-  return code+'!';
+Blockly.JavaScript['main'] = function(block) {
+    var statements_main = Blockly.JavaScript.statementToCode(block, 'main');
+    // TODO: Assemble JavaScript into code variable.
+    var dict12 = {};
+    a = statements_main.split("!");
+    function jsonConcat(o1, o2) {
+        for (var key in o2) {
+            o1[key] = o2[key];
+        }
+        return o1;
+    }
+    var config = {};
+    var final = [];
+    for(i = 0; i< a.length-1; i++) {
+
+        q = JSON.parse(a[i]);
+        config = jsonConcat(config, q);
+        final.push(q);
+
+    }
+    dict12['kedge'] = final;
+ //   return dict12;
+    var code = JSON.stringify(dict12);
+    return code;
+};
+
+/////////////////////
+Blockly.JavaScript['name'] = function(block) {
+    var text_name = block.getFieldValue('name');
+    var dict = {};
+    dict["name"] = text_name;
+    // TODO: Assemble JavaScript into code variable.
+    var code = JSON.stringify(dict);
+    return code+"!";
+};
+/////////////////////////
+
+Blockly.JavaScript['containers'] = function(block) {
+    var statements_containers = Blockly.JavaScript.statementToCode(block, 'containers');
+    // TODO: Assemble JavaScript into code variable.
+   // console.log(statements_containers);
+    var dict = {};
+
+
+         a = statements_containers.split("!");
+    function jsonConcat(o1, o2) {
+        for (var key in o2) {
+            o1[key] = o2[key];
+        }
+        return o1;
+    }
+    var config = {};
+    for(i = 0; i< a.length-1; i++){
+
+
+        q = JSON.parse(a[i]);
+        config = jsonConcat(config, q);
+
+
+    }
+   // console.log("container is", config);
+      //   a = JSON.parse(a);
+    dict["containers"] = [config];
+
+    var code =JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['image'] = function(block) {
+    var text_image = block.getFieldValue('image');
+    // TODO: Assemble JavaScript into code variable.
+    var dict = {};
+    dict["image"] = text_image;
+    var code =JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['controller'] = function(block) {
+    var dropdown_service_type = block.getFieldValue('controller');
+    // TODO: Assemble JavaScript into code variable.
+    var dict = {};
+    dict["controller"] = dropdown_service_type;
+    var code = JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['container_name'] = function(block) {
+    var text_container_name = block.getFieldValue('container_name');
+    // TODO: Assemble JavaScript into code variable.
+    var dict = {};
+    dict["name"] = text_container_name;
+    var code =JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['services'] = function(block) {
+    var statements_services = Blockly.JavaScript.statementToCode(block, 'services');
+    // TODO: Assemble JavaScript into code variable.
+    var dict = {};
+    a = statements_services.split("!");
+    function jsonConcat(o1, o2) {
+        for (var key in o2) {
+            o1[key] = o2[key];
+        }
+        return o1;
+    }
+    var config = {};
+    for(i = 0; i< a.length-1; i++){
+
+
+        q = JSON.parse(a[i]);
+        config = jsonConcat(config, q);
+
+
+    }
+    dict["services"] = [config];
+    var code =JSON.stringify(dict);
+    return code+"!";
+
+};
+
+Blockly.JavaScript['service_type'] = function(block) {
+    var dropdown_service_type = block.getFieldValue('service_type');
+    // TODO: Assemble JavaScript into code variable.
+   var dict = {};
+    dict["type"] = dropdown_service_type;
+    var code = JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['service_name'] = function(block) {
+    var text_service_name = block.getFieldValue('service_name');
+    // TODO: Assemble JavaScript into code variable.
+    var dict = {};
+    dict["name"] = text_service_name;
+    var code = JSON.stringify(dict);
+    return code+"!";
+};
+
+Blockly.JavaScript['ports_mappings'] = function(block) {
+    var statements_ports = Blockly.JavaScript.statementToCode(block, 'ports_mappings');
+    a = statements_ports.split("!");
+    a.splice(a.length-1, 1);
+    a[0] = a[0].trim();
+    var dict = {};
+    dict['PortMappings'] = a;
+
+    var code = JSON.stringify(dict);
+    return code+'!';
+};
+
+Blockly.JavaScript['env_mappings'] = function(block) {
+    var statements_ports = Blockly.JavaScript.statementToCode(block, 'env_mappings');
+    a = statements_ports.split("!");
+    a.splice(a.length-1, 1);
+    a[0] = a[0].trim();
+    var dict = {};
+    dict['PortMappings'] = a;
+
+    var code = JSON.stringify(dict);
+    return code+'!';
 };
 
 Blockly.JavaScript['ports'] = function(block) {
-  var statements_ports = Blockly.JavaScript.statementToCode(block, 'ports');
-             a = statements_ports.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['ports'] = a;
+    var statements_ports = Blockly.JavaScript.statementToCode(block, 'ports');
+    a = statements_ports.split("!");
+    a.splice(a.length-1, 1);
+    a[0] = a[0].trim();
+    var dict = {};
+    dict['PortMappings'] = a;
 
-   var code = JSON.stringify(dict);
-  return code+'!';
+    var code = JSON.stringify(dict);
+    return code+'!';
 };
 
+Blockly.JavaScript['label_output'] = function(block) {
+    var text_label_output = block.getFieldValue('label_output');
+    var code = text_label_output.trim();
+    return code+'!';
+};
 
 Blockly.JavaScript['page_text'] = function(block) {
-  var text_text = block.getFieldValue('page_text');
-  var code = text_text;
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['restart'] = function(block) {
-  var text_restart = block.getFieldValue('restart');
-    var dict = {};
-    dict['restart'] = text_restart;
-  var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-//
-Blockly.JavaScript['container_name'] = function(block) {
-  var value_container_name = Blockly.JavaScript.valueToCode(block, 'container_name', Blockly.JavaScript.ORDER_ATOMIC);
-
-     value_container_name = value_container_name.replace(/[{()}]/g, '');
-  var dict = {};
-   dict['container_name'] = value_container_name;
-   var code = JSON.stringify(dict);
-   return code+'!';
+    var text_text = block.getFieldValue('page_text');
+    var code = text_text;
+    return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['labels'] = function(block) {
@@ -115,77 +234,10 @@ Blockly.JavaScript['labels'] = function(block) {
   return code+'!';
 };
 
-Blockly.JavaScript['label_output'] = function(block) {
-  var text_label_output = block.getFieldValue('label_output');
-  var code = text_label_output.trim();
-  return code+'!';
-};
-
-Blockly.JavaScript['environment'] = function(block) {
-  var statements_environment = Blockly.JavaScript.statementToCode(block, 'environment');
-             a = statements_environment.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['env'] = a;
-
-   var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-Blockly.JavaScript['args'] = function(block) {
-  var statements_args = Blockly.JavaScript.statementToCode(block, 'args');
-             a = statements_args.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['args'] = a;
-
-   var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-Blockly.JavaScript['env_file'] = function(block) {
-  var value_env_file = Blockly.JavaScript.valueToCode(block, 'env_file', Blockly.JavaScript.ORDER_ATOMIC);
-    value_env_file = value_env_file.replace(/[{()}]/g, '');
- var dict = {};
-  dict['env_file'] = value_env_file;
-  var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-Blockly.JavaScript['links'] = function(block) {
-  var statements_links = Blockly.JavaScript.statementToCode(block, 'links');
-             a = statements_links.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['links'] = a;
-
-   var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-Blockly.JavaScript['cap_add'] = function(block) {
-  var statements_links = Blockly.JavaScript.statementToCode(block, 'cap_add');
-             a = statements_links.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['cap_add'] = a;
-
-   var code = JSON.stringify(dict);
-  return code+'!';
-};
-
-Blockly.JavaScript['cap_drop'] = function(block) {
-  var statements_links = Blockly.JavaScript.statementToCode(block, 'cap_drop');
-             a = statements_links.split("!");
-             a.splice(a.length-1, 1);
-              a[0] = a[0].trim();
-            var dict = {};
-            dict['cap_drop'] = a;
-
-   var code = JSON.stringify(dict);
-  return code+'!';
+Blockly.JavaScript['env'] = function(block) {
+    var text_key = block.getFieldValue('key');
+    var text_value = block.getFieldValue('value');
+    // TODO: Assemble JavaScript into code variable.
+    var code = '...;\n';
+    return code;
 };
